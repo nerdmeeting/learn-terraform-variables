e7f739ffe3f3add2d39e7b7c141295259e4cd371
Nathan Bills <nathan@nerdmeeting.com>
2024-12-24 19:03:51 -0700

    Set the CIDR block for the VPC with a new variable vpc_cidr_block.

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.7.0"
 
-  cidr = "10.0.0.0/16"
+  cidr = var.vpc_cidr_block
 
   azs             = data.aws_availability_zones.available.names
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
--- variables.tf
+++ variables.tf
@@ -7,3 +7,9 @@ variable "aws_region" {
   type = string
   default = "us-west-2"
 }
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type = string
+  default = "10.0.0.0/16"
+}
